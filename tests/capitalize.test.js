8c2bc23cb62d081060cf273c800a5def488c78fa
Konstantin Popov <130471029+costachille@users.noreply.github.com>
2024-08-28 23:42:40 +0800

    replacing manual assertion with module assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,7 +1,9 @@
+// Ручные утверждения
+
 // нажмите кнопку run чтобы запустить тесты
 // попробуйте изменять код функции / тестов, запуская проверки заново
 
-import { capitalize } from '../src/capitalize.js';
+/* import { capitalize } from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
   throw new Error('Функция работает неверно!');
@@ -10,5 +12,23 @@ if (capitalize('hello') !== 'Hello') {
 if (capitalize('') !== '') {
   throw new Error('Функция работает неверно!');
 }
+
+console.log('Все тесты пройдены!'); */
+
+// Модуль assets
+
+import { strict as assert } from 'assert';
+
+assert.strictEqual('hello', 'hello');
+
+assert.strictEqual({}, {}); // ошибка
+assert.deepStrictEqual({}, {});
+
+assert.deepStrictEqual({ key: 'value' }, { key: 'value' });
+assert.deepEqual({ key: 'value' }, { key: 'value' });
+assert.deepEqual({ key: 'value' }, { key: 'another value' }); // ошибка
+
+assert.notStrictEqual('hello', 'world');
+assert.notDeepStrictEqual({ a: 1 }, { a: '1' });
 
 console.log('Все тесты пройдены!');
